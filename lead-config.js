c9ef53e1233f99d699a8277636466f5ac9a290de
Mrkabu80 <mr.kabu80@gmail.com>
2026-07-07 09:56:54 +0200

    Add files via upload

--- lead-config.js
+++ lead-config.js
@@ -2,4 +2,4 @@
 // Paste your deployed Google Apps Script Web App URL between the quotes below.
 // Example:
 // window.LUMIAN_LEAD_WEBHOOK = "https://script.google.com/macros/s/AKfycb.../exec";
-window.LUMIAN_LEAD_WEBHOOK = "";
+window.LUMIAN_LEAD_WEBHOOK = "https://script.google.com/macros/s/AKfycbzE4gou4eqYLhpS_Ap4oDTMDHQBqk1KC9m6XXBJCP2VefN0AKWSPhH6pcWzrBaMftRiVg/exec";
